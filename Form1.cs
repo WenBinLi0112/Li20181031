70e3e8e8d2b693653030a8d6a766afcf38ef698f
WenBinLi0112 <1192854597@qq.com>
2018-10-31 19:17:13 +0800

    提交Form1.cs文件

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MongoDBTest
+{
+    public partial class Form1 : Form
+    {
+        MongoDBHelper MDB = new MongoDBHelper();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            MDB.ConnectDB();
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            MDB.Insert();
+        }
+
+        private void Read_Click(object sender, EventArgs e)
+        {
+            MDB.Query();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            MDB.Delete();
+
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            MDB.Update();
+        }
+
+
+    }
+}
